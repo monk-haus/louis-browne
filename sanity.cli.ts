ce1ae5027643bd21ffdf311e9e3091d501446156
monk-haus <hello@monk.haus>
2026-04-01 19:24:22 +0300

    fix

---
+++ sanity.cli.ts
@@ -0,0 +1,12 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "26rs8n8l",
+    dataset: "production",
+  },
+  studioHost: "louis-browne",
+  deployment: {
+    appId: "pmaagfk93geum35olgwta8gb",
+  },
+});
